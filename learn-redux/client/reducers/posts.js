1aed82ac4def1c383648228e8af6304b91a727ee
Andrew <andy.birks@gmail.com>
2019-06-02 12:16:25 +0100

    #12 — Updating State with Reducers

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -3,13 +3,25 @@
 //  1 - the action
 //  2-  a copy of the current state
 
-function posts(state=[],action){
-    console.log('the post will change');
-    
-console.log(state,action);
+function posts(state = [], action) {
 
-return state;
-
+    switch(action.type){
+        case 'INCREMENT_LIKES':
+        //  Get the key of what was clicked
+        const i = action.index;
+        //  Return the updated state
+        return [
+          ...state.slice(0,i), // before the one we are updating
+          {
+            ...state[i], // Take a copy of the state in pos[i]
+            likes: state[i].likes +1 // Alter the property
+          },
+          ...state.slice(i+1), // after the one we are updating
+        ]
+        
+        default: 
+            return state
+    }
 }
 
 export default posts;
