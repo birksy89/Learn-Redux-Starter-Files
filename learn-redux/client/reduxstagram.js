4ce97ae5a6a5a688876fffc74d4d1484e9a79674
Andrew <andy.birks@gmail.com>
2019-06-02 11:29:22 +0100

    #10 — Accessing Dispatch and State with Redux

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute } from 'react-router';
 import {Provider} from 'react-redux';
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
@@ -17,7 +17,7 @@ const router = (
   <Provider store={store}>
     
   <Router history={history}>
-    <Route path="/" component={Main}>
+    <Route path="/" component={App}>
       <IndexRoute component={PhotoGrid} />
       <Route path="/view/:postId" component={Single} />
     </Route>
